f17c8775878966ac18c9b5136ac03e0239c188c8
Knakworstje <92457091+Knakworstje@users.noreply.github.com>
2024-02-21 14:13:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,12 +7,11 @@ const ec = new EC('secp256k1');
 const keypair = ec.keyFromPrivate(JSON.parse(fs.readFileSync('./keypair.json')).privateKey);
 
 const chain = new Blockchain;
-chain.addBlock(new Block('hi'));
-chain.addBlock(new Block('test'));
-chain.addBlock(new Block('hello world!'));
-
-console.log(chain.isChainValid());
-
-chain.chain[2].transactions = 'tamepered with';
 
 module.exports.chain = chain;
+module.exports.Contract = Contract;
+module.exports.Transaction = Transaction;
+module.exports.connect = connect;
+module.exports.emitTx = emitTx;
+module.exports.emitContract = emitContract;
+module.exports.keypair = keypair;
